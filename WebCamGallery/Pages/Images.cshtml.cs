e31885b8708089a08be20de258bcc0ce07b1cc1a
ChaosEngine <andrzej.pauli@gmail.com>
2018-01-30 23:03:21 +0100

    images handler

---
+++ WebCamGallery/Pages/Images.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace WebCamGallery.Pages
+{
+	public class ImagesModel : PageModel
+	{
+		private readonly string _imageDirectory;
+
+		public ImagesModel(IConfiguration configuration)
+		{
+			var imageDirectory = configuration["ImageDirectory"];
+			_imageDirectory = imageDirectory;
+		}
+
+		public IActionResult OnGet(string fileName)
+		{
+			if (string.IsNullOrEmpty(fileName)) return NotFound();
+
+			if (Directory.Exists(_imageDirectory))
+			{
+				var path = Path.Combine(_imageDirectory, fileName);
+				if (System.IO.File.Exists(path))
+				{
+					//var fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+					return base.PhysicalFile(path, "image/jpg"/*, fileName*/);
+				}
+			}
+			return NotFound();
+		}
+	}
+}
